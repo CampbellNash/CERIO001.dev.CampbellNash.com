d1df9028848f1b9529976fc02827ece24c1afc92
Stephen Davidson <stephen.davidson@campbellnash.com>
2013-05-03 11:18:54 +0100

    Converted login to a user control

--- Default.aspx
+++ Default.aspx
@@ -1,52 +1,14 @@
 ﻿<%@ Page Title="" Language="VB" MasterPageFile="~/masterpages/landing.master" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
 
+<%@ Register src="controls/homepageLogin.ascx" tagname="homepageLogin" tagprefix="uc1" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="cpcHeroLeft" Runat="Server">
      <h1>CERICO / A Compliance and Due Diligence solution</h1>
             <p>Ensuring compliance with UK and International Anti-Bribery and Corruption legislation can be a challenge. In particular, you need to be sure that your supply chain is compliant with your company policies</p>
             <p><a href="#" class="btn btn-info btn-large">Learn more &raquo;</a></p>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cphHeroRight" Runat="Server">
-     <div class="hpform-signin">
-         <asp:Label ID="lblLoginError" runat="server" CssClass="requried" />
-          <asp:Panel ID="panFormWrapper" runat="server" defaultbutton="btnLogin">
-                        <label>Username</label>
-                        <asp:TextBox ID="txtUserName" runat="server" CssClass="input-block-level" placeholder="Enter Username" TabIndex="1"></asp:TextBox>
-                        <asp:RequiredFieldValidator ID="rfvEmailAddress" ControlToValidate="txtUserName" CssClass="error" ForeColor="red" runat="server" Display="Dynamic" ErrorMessage="please enter your email address or username"></asp:RequiredFieldValidator>
-                        <label>Password (<asp:LinkButton ID="btnForgot" runat="server" Text="forgot password!" CausesValidation="false" />)</label>
-                        <asp:TextBox ID="txtPassword" runat="server" CssClass="input-block-level" placeholder="Password" TabIndex="2" TextMode="Password" />
-                        <asp:RequiredFieldValidator ID="rfvPassword" ControlToValidate="txtPassword" CssClass="error" ForeColor="red" runat="server" Display="Dynamic" ErrorMessage="please enter your password"></asp:RequiredFieldValidator>
-                        <asp:Button ID="btnLogin" runat="server" CssClass="btn btn-large btn-success" Text="Login to CERICO &raquo;" />
-                        <asp:Panel ID="panWarning" runat="server" Visible="false" CssClass="alert alert-block gaptop">
-                            <button type="button" class="close" data-dismiss="alert">&times;</button>
-                            <asp:Label ID="lblWarning" runat="server" />
-                        </asp:Panel>                      
-                        <label><a href="register.aspx">Not Registered? Sign up now</a></label>
-                        <label><a href="pricing.aspx" >See plans and pricing</a></label>              
-          </asp:Panel>
-          <asp:Panel ID="panForgot" runat="server" Visible="false">
-                <h4>Enter your email address that you registered with into the box below.</h4>
-                <label>Email Address:</label>
-                <asp:TextBox ID="txtEmailAddress" runat="server" TextMode="SingleLine" CssClass="input-block-level" MaxLength="50" />
-                <asp:RequiredFieldValidator ID="rfvEmail" runat="server" ControlToValidate="txtEmailAddress" Display="Dynamic" ForeColor="#ffffcc" ValidationGroup="Forgot" ErrorMessage="Please enter email address" />
-               <br />
-                <asp:Button ID="btnSend" Text="Send password &raquo;" runat="server" CssClass="btn btn-large btn-success" ValidationGroup="Forgot" AlternateText="Send password" ToolTip="Send password" ImageAlign="Right" />&nbsp;&nbsp;<asp:Button ID="btnCancelForgot" runat="server" Text="Cancel" CssClass="btn btn-danger btn-large" AlternateText="Cancel" ToolTip="Cancel" ImageAlign="Right"  />
-            </asp:Panel>
-            
-            <asp:Panel ID="panConfirmSend" runat="server" Visible="false">
-                <p>Thank you!</p>
-                <p>We have sent a mail containing your login credentials to the address you entered, please check for this mail in all your mailbox folders and follow the instructions given.</p>
-            </asp:Panel>
-         <asp:Panel ID="panNotActivated" runat="server" Visible="false" CssClass="alert alert-block gaptop">
-             <asp:Button ID="btnReturn" runat="server" Text="&times;" type="button" class="close"  />  
-              According to our records you have a pending account with us.
-                Your account is still awaiting approval, when this happens you will receive a mail confirming your account is ready to use. Click the button below to return to the login page.
-             
-            </asp:Panel>
-                        
-                       
-                       
-                        
-     </div>
+     <uc1:homepageLogin ID="homepageLogin1" runat="server" />
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="cphLowerSection" Runat="Server">
      <div class="row-fluid">
---
+++ controls/homepageLogin.ascx.vb
@@ -0,0 +1,119 @@
+﻿Option Strict Off
+Option Explicit On
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'  Page Title       : homepageLogin.ascx.vb             '
+'  Description      : Login user control                ' 
+'  Author           : Stephen Davidson                  '
+'  Creation Date    : 03 May 2013                       '
+'  Version No       : 1                                 '
+'  Revision         :                                   '
+'  Revision Reason  :              		                '
+'  Revisor          :            		  			    '
+'  Date Revised     :                     			    '  
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Imports System
+Imports System.Web
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.HtmlControls
+Imports System.Net
+Imports System.IO
+Imports System.Configuration
+Imports System.Net.Mail
+Imports Microsoft.VisualBasic
+Imports MasterClass
+Imports System.Security.Cryptography.X509Certificates
+Imports System.Net.Security
+
+Partial Class controls_homepageLogin
+    Inherits System.Web.UI.UserControl
+
+    Protected Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        'We can try to log our user in now
+        Dim loginDetails As DataSet = NashBLL.LoginUser(txtUserName.Text, txtPassword.Text)
+        Dim dr As DataRow = loginDetails.Tables(0).Rows(0)
+        Select Case CInt(dr("Result"))
+            Case 0
+                'We can login this user now
+                Session("UserLoggedIn") = True
+                Session("FirstName") = dr("FirstName")
+                Session("ContactID") = CInt(dr("ContactID"))
+                Session("Surname") = dr("Surname")
+                Session("EmailAddress") = dr("EmailAddress")
+                'Now we can redirect to the home page
+                Response.Redirect("~/mycerico.aspx")
+            Case -1
+                'Incorrect user name
+                panWarning.Visible = True
+                lblWarning.Text = "Incorrect username or password, please check your details and try again [1]"
+            Case -2
+                panWarning.Visible = True
+                lblWarning.Text = "Incorrect username or password, please check your details and try again [2]"
+            Case Else
+                'Incorrect password
+                panWarning.Visible = True
+                lblWarning.Text = "You have not verified your account, please check for your verification mail or contact us for more help [3]"
+        End Select
+
+    End Sub
+
+    Protected Sub btnForgot_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnForgot.Click
+        'User requesting to be sent their username and password
+        lblWarning.Text = ""
+        panFormWrapper.Visible = False
+        panForgot.Visible = True
+        txtEmailAddress.Text = ""
+    End Sub
+
+    Protected Sub btnCancelForgot_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCancelForgot.Click, btnReturn.Click
+        'User is cancelling password request
+        panFormWrapper.Visible = True
+        panForgot.Visible = False
+        lblLoginError.Text = ""
+        panWarning.Visible = False
+        panNotActivated.Visible = False
+    End Sub
+
+    Protected Sub btnSend_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSend.Click
+        Dim MailBody As String = ""
+        Dim Subject As String = ""
+        Dim RecipientList As String = ""
+        'Try
+        Dim UserData As String = NashBLL.SendUserAndPassword(txtEmailAddress.Text)
+        If UserData = "-1" Then
+            'This account has not been verified yet so halt and advise
+            panForgot.Visible = False
+            panNotActivated.Visible = True
+            Return
+        ElseIf UserData = "-2" Then
+            'Account was not found
+            lblLoginError.Text = "Sorry we were unable to find an account matching the email address you entered. Please verify &amp; try again."
+            Return
+        Else
+            Dim MyArray As Array = Split(UserData, ",")
+            'We found a user for the entered email address so we can send the mail now
+            MailBody = "Dear " & MyArray(2) & "," & vbCrLf & vbLf & _
+            "Please find your login details for our site below." & vbCrLf & vbLf & _
+            "Username: " & MyArray(0) & vbCrLf & _
+            "Password: " & MyArray(1) & vbCrLf & vbLf & _
+            "Visit 'web site address here' and login with these details." & vbCrLf & vbLf & _
+            "If you have any further problems then please consult your support team."
+            Subject = "Login details from CERICO"
+            RecipientList = MyArray(0)
+            'Now send the mail
+            NashBLL.SendMail(RecipientList, "", "", MailBody, Subject, "", False)
+            'Inform the user now
+            lblLoginError.Text = ""
+            panForgot.Visible = False
+            panConfirmSend.Visible = True
+        End If
+
+        'Catch
+
+        'End Try
+    End Sub
+
+
+End Class
